01176db63dc4825922cafe65c7fd548e4597ac9e
UgurAbasov <ugurabbasov65@gmail.com>
2023-12-29 19:59:44 +0400

    added cors protection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = ['https://tealcian-frontend.vercel.app'];
+  app.use(cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  }));
   app.enableCors()
   await app.listen(process.env.PORT || '0.0.0.0');
 }
