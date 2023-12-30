89dbf7c34ce7dfb716770f1869db1cde28ec4247
UgurAbasov <ugurabbasov65@gmail.com>
2023-12-30 18:22:22 +0400

    fixed bad code

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,10 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const allowedOrigins = ['https://tealcian-frontend.vercel.app'];
-  app.use(cors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204,
-  }));
-  app.enableCors()
+  app.enableCors({
+    origin: 'https://tealcian-frontend.vercel.app',
+    credentials: true
+  })
   await app.listen(process.env.PORT || '0.0.0.0');
 }
 bootstrap();
