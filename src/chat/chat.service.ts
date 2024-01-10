bfa0efc1b65749e1e9ec8b5a4f711678654a704d
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-10 20:26:49 +0400

    first commit

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -247,12 +247,10 @@ export class ChatService {
                         message: true
                     }
                 })
-
+                usersPrivates.push(findPrivate)
                 for(let j = 0; j < findPrivate.length;j++){
                     const lastMassage = findPrivate[j].message
-                    if(lastMassage[lastMassage.length - 1] !== null){
-                        usersPrivates.push(lastMassage[lastMassage.length - 1])
-                    }
+                    usersPrivates.push(lastMassage[lastMassage.length - 1].body)
                 }
               }
               return usersPrivates
