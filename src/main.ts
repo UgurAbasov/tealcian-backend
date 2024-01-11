e84cbd3ea14a2f0b0964d196ea7b2813ae10587f
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-11 21:24:24 +0400

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: 'https://tealcian-frontend.vercel.app',
-  //   credentials: true
-  // })
+  app.enableCors({
+    origin: 'https://tealcian-frontend.vercel.app',
+    credentials: true
+  })
   await app.listen(process.env.PORT || '0.0.0.0');
 }
 bootstrap();
