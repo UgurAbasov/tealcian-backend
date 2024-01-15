44493079cb850829be89443670afcbdcccfb1163
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-16 01:27:04 +0400

    added component

--- src/chat/events/events.gateway.ts
+++ src/chat/events/events.gateway.ts
@@ -131,8 +131,7 @@ async deleteMessage(@ConnectedSocket() client: Socket, @MessageBody() message: D
                 body: message.message,
                 createdAt: message.time,
                 privateId: getPrivate.id,
-                userId: message.userId
-            }
+             }
         })
         if(!getMessage){
             return 'error'
