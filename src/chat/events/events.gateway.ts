3b28a0a224f463ea12f5dce6a6f20f3553250769
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-19 16:02:32 +0400

    added component

--- src/chat/events/events.gateway.ts
+++ src/chat/events/events.gateway.ts
@@ -8,9 +8,8 @@ import { OnGatewayConnection } from "@nestjs/websockets";
 import { PrismaService } from 'src/prisma/prisma.service';
 import groupMessagesByDate from 'src/utils/separateTime';
 import {build} from 'schemapack'
-
-@WebSocketGateway({
-    cors: { origin: "url", methods: ["GET", "POST"] }})
+import schemaPackParser from 'schemapack'
+@WebSocketGateway({cors: { origin: "https://tealcian-frontend.vercel.app/chat", methods: ["GET", "POST"] }, parser: schemaPackParser})
   export class EventGateway implements OnGatewayConnection, OnGatewayDisconnect, OnModuleInit {
     @WebSocketServer()
     server: Server
